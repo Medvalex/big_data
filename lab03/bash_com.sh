b5feb57c33e82f7181c8fe82e5b440bbaacca761
Alexandr Medvedev <medvedv.audit@gmail.com>
2020-04-16 03:21:45 +0300

    update

---
+++ lab03/bash_com.sh
@@ -0,0 +1,6 @@
+#* bash code for MR
+
+hadoop jar ~/hadoop-streaming.jar -D mapred.reduce.tasks=0 -input /user/alexander.medvedev/lab03_in/ -output /user/alexander.medvedev/lab03/result/ -mapper ./mapper.py -file "mapper.py"
+
+hadoop fs -mkdir /user/alexander.medvedev/lab03_in
+
